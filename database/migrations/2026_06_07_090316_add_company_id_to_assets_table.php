8f83887b71526f6d2cfa8b1960b9095d72e3545a
widia2807 <136869566+widia2807@users.noreply.github.com>
2026-06-07 16:04:30 +0700

    new migration

---
+++ database/migrations/2026_06_07_090316_add_company_id_to_assets_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+{
+    Schema::table('assets', function (Blueprint $table) {
+        $table->unsignedBigInteger('company_id')->nullable()->after('id');
+    });
+}
+
+public function down()
+{
+    Schema::table('assets', function (Blueprint $table) {
+        $table->dropColumn('company_id');
+    });
+}
+};
